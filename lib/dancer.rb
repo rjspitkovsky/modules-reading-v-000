6e80f79333b68689c96e6d3f6dd603ac3db0f4eb
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-02-08 19:02:50 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,11 +3,11 @@ require_relative './dance_module.rb'
 
 class Dancer
   extend MetaDancing
-  # include Dance
-  #
-  # attr_accessor :name
-  #
-  # def initialize(name)
-  #   @name = name
-  # end
+  include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
 end
